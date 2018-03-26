5d753106e3dcf34ab63820033848878109dc6274
Daniel Garnier-Moiroux <git@garnier.wf>
2018-03-26 07:03:06 +0200

    Domain lister implementation

--- domain-picker/src/main/kotlin/wf/garnier/domainpicker/DomainLister.kt
+++ domain-picker/src/main/kotlin/wf/garnier/domainpicker/DomainLister.kt
@@ -1,28 +1,18 @@
 package wf.garnier.domainpicker
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
-import java.net.URI
+import wf.garnier.domain.Domain
 
 @RestController
-class DomainLister(@Value("\${domains.base_url:}") val baseUrl: String) {
+class DomainLister(@Value("\${extensions:}") val extensions: Collection<String>) {
 
+    // TODO: Make some unavaialble ?
     @GetMapping("/domains")
-    fun getDomains(@RequestParam("search") searchTerm: String): Flux<String> {
-        val url =
-            "$baseUrl?currency=EUR&lang=en&page=1&per_page=30&search=$searchTerm"
-
-        return WebClient.create()
-            .get()
-            .uri(URI(url))
-            .accept(MediaType.TEXT_EVENT_STREAM)
-            .header("Referer", "https://shop.gandi.net/en/domain/suggest")
-            .exchange()
-            .flatMapMany { r -> r.bodyToFlux(String::class.java) }
-    }
+    fun getDomains(@RequestParam("search") searchTerm: String) =
+        Flux.fromIterable(extensions)
+            .map { Domain(searchTerm, it, true) }
 }
