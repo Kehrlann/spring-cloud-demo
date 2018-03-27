97d417b31985a3e45452d40d80a53f8208bdb406
Daniel Garnier-Moiroux <git@garnier.wf>
2018-03-27 21:44:31 +0200

    Package domain-lib and add whois model

--- domain-lib/src/main/kotlin/wf/garnier/domain/Models.kt
+++ domain-lib/src/main/kotlin/wf/garnier/domain/Models.kt
@@ -1,7 +1,11 @@
 package wf.garnier.domain
 
+import java.time.LocalDate
+
 data class Domain(
     val name: String,
     val extension: String,
     val available: Boolean
 )
+
+data class WhoIs(val company: String, val contact: String, val validUntil: LocalDate)
