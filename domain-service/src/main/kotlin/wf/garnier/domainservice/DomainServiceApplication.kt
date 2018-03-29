1093dcce44e223c853cce47e6b9729a9a85ef8c9
Daniel Garnier-Moiroux <git@garnier.wf>
2018-03-29 21:51:10 +0200

    domain-service: add eureka client

--- domain-service/src/main/kotlin/wf/garnier/domainservice/DomainServiceApplication.kt
+++ domain-service/src/main/kotlin/wf/garnier/domainservice/DomainServiceApplication.kt
@@ -2,8 +2,10 @@ package wf.garnier.domainservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class DomainServiceApplication
 
 fun main(args: Array<String>) {
