8bbfccddfc045e89bf6e8b3c9b8c14132710a0a8
Daniel Garnier-Moiroux <git@garnier.wf>
2018-03-30 19:06:18 +0200

    domain-lib: default values for Domain model
    - Thanks, JAXB ! :)

--- domain-lib/src/main/kotlin/wf/garnier/domain/Models.kt
+++ domain-lib/src/main/kotlin/wf/garnier/domain/Models.kt
@@ -3,9 +3,9 @@ package wf.garnier.domain
 import java.time.LocalDate
 
 data class Domain(
-    val name: String,
-    val extension: String,
-    val available: Boolean
+    val name: String = "",
+    val extension: String = "",
+    val available: Boolean = false
 )
 
 data class WhoIs(val company: String, val contact: String, val validUntil: LocalDate)
