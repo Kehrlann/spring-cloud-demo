704ec09f6d93d3ada065b2d0107b277088330a61
Daniel Garnier-Moiroux <git@garnier.wf>
2018-03-31 20:04:54 +0200

    domain-service: improve integration tests
    By fixing the domain extensions in the properties
    of the integration test

--- domain-service/src/test/kotlin/wf/garnier/domainservice/DomainControllerTest.kt
+++ domain-service/src/test/kotlin/wf/garnier/domainservice/DomainControllerTest.kt
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -28,7 +30,8 @@ class DomainControllerTest {
 }
 
 @RunWith(SpringRunner::class)
-@WebMvcTest(DomainServiceConfiguration::class, DomainController::class)
+@SpringBootTest(properties = ["domain.extensions=com,org,io"])
+@AutoConfigureMockMvc
 class DomainControllerControllerTest {
 
     private val expectedDomains = listOf(
