565bfd576dca928fba0411223de642bc816662df
Daniel Garnier-Moiroux <git@garnier.wf>
2018-04-04 07:17:24 +0200

    pricing-service: todo for more dynamic pricing (based on extensions ?)

--- pricing-service/src/main/kotlin/wf/garnier/pricingservice/PricingController.kt
+++ pricing-service/src/main/kotlin/wf/garnier/pricingservice/PricingController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class PricingController {
 
+    // FIXME : more dynamic price ? (through configuration ?)
     @GetMapping("/api/price")
     fun getPrice(@RequestParam("domain") domain: String) = 5 + domain.length
 }
