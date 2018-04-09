8b7c3c497b7ff3a56e5b428ac3ea7aad0341810e
Daniel Garnier-Moiroux <git@garnier.wf>
2018-04-09 06:58:42 +0200

    domain-picker: remove all code before presentation

--- domain-picker/src/main/kotlin/wf/garnier/domainpicker/DomainPickerApplication.kt
+++ domain-picker/src/main/kotlin/wf/garnier/domainpicker/DomainPickerApplication.kt
@@ -2,28 +2,9 @@ package wf.garnier.domainpicker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
-import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.context.annotation.Bean
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
-@EnableFeignClients
-class DomainPickerApplication(val restTemplateBuilder: RestTemplateBuilder,
-                              val lbFunction: LoadBalancerExchangeFilterFunction) {
-    @Bean
-    @LoadBalanced
-    fun restTemplate(): RestTemplate = restTemplateBuilder.build()
-
-    @Bean
-    fun webClient(): WebClient = WebClient.builder().filter(lbFunction).build()
+class DomainPickerApplication {
 }
 
 fun main(args: Array<String>) {
